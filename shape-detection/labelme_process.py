7f940b67289a4b1c27393d3546b7d2f37e2c7034
Zhiwen Fu <fuzhiwen@iqubic.net>
2019-03-20 22:02:00 +0800

    Mar 20 inter

--- shape-detection/labelme_process.py
+++ shape-detection/labelme_process.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-import math
+from typing import (
+    Any,
+    List,
+    Mapping,
+    Tuple,
+    Union,
+)
 import json
 from operator import (
     concat,
+    getitem,
     itemgetter,
+    methodcaller,
 )
 from functools import (
     reduce,
@@ -16,16 +24,16 @@ from itertools import (
     chain,
 )
 import numpy as np
-from labelme import utils
 from darwinutils.fn import (
+    f_compose_l,
     f_compose_r,
     f_group,
+    f_flip_call,
 )
-from imutils import *
 from darwinutils.log import get_task_logger
-#from darwinutils.helper import (
-#    get_dict_filter_function_per_keys,
-#)
+from darwinutils.helper import (
+    get_dict_filter_function_per_keys,
+)
 
 
 logger = get_task_logger(__name__)
@@ -42,109 +50,241 @@ f_dict_filter_func_per_AOI_keys = get_dict_filter_function_per_keys(
 
 
 def load_labeled_img(img_path: str):
+    from labelme.utils import img_b64_to_arr
+
     with open(img_path, "r") as fd:
         img_data = json.load(fd)
+    keys = set(img_data.keys()) - {"imageData", "shapes"}
     return f_compose_r(
         # f_dict_filter_func_per_AOI_keys,
         f_group(
             f_compose_r(
                 itemgetter("imageData"),
-                utils.img_b64_to_arr,
+                img_b64_to_arr,
             ),
             itemgetter(
                 "imageHeight",
                 "imageWidth",
             ),
             itemgetter("shapes"),
+            # template for sub-shapes by excluding "imageData" and "shapes"
+            get_dict_filter_function_per_keys(keys),
         ),
         tuple,
     )(img_data)
 
 
-def calc_min_area_rect(points):
+def _warp_transform(f_wrap, f_transform, m, mat=None, points=None, *args, **kwargs):
+    if mat is not None:
+        if len(args) == 0:
+            if "dsize" not in kwargs:
+                args = [mat.shape[:2][::-1]]
+            else:
+                args = [kwargs.pop("dsize")]
+        mat = f_wrap(mat, m, *args, **kwargs)
+
+    if points is not None:
+        transform_kwargs = {}
+        if "dst" in kwargs:
+            transform_kwargs["dst"] = kwargs["dst"]
+        lens = list(map(len, points))
+        slices = reduce(lambda y, x: y+[slice(y[-1].stop, y[-1].stop+x)], lens, [slice(0, 0)])[1:]
+        points = f_compose_r(
+            # expand points array
+            partial(np.concatenate, axis=0),
+            # reshape to satisfy f_transform
+            partial(np.expand_dims, axis=1),
+            # enforce type convert to satisfy f_transform
+            methodcaller("astype", np.float32),
+            # fire...
+            partial(f_flip_call, f_transform, m, **transform_kwargs),
+            # reshape to align with normal "point"
+            partial(np.squeeze, axis=1),
+            # reshape to align with input
+            partial(partial, getitem),
+            partial(f_flip_call, map, slices),
+            list,
+        )(points)
+    return mat, points
+
+
+def warp_affine_transform(*args, **kwargs):
     """
-    计算多边形(polygon)对应最小矩形框(minAreaRect)
-    :param points:
-    :param rotated_rect:
-    :return: cv2::RotatedRect
-    """
-    import cv2
-
-    return cv2.minAreaRect(points)
-
-
-def _expand_bbox_to_rect(lr, rd):
-    return lr, (lr[0], rd[1]), rd, (rd[0], lr[1])
-
-
-def convert_rotated_rect_to_bbox(rotated_rect):
-    center, (w, h), angle = rotated_rect
-    angle *= math.pi/90.0
-    a = -math.sin(angle)
-    b = math.cos(angle)
-    _w = abs(w*b + h*a)
-    _h = abs(w*a + h*b)
-    return center, (_w, _h), 0.0
-
-
-def rotate_img(img: np.array, angle: float, scale: float = 0.0, center = None, bboxes = None, points = None):
-    """
-    Rotate img and its associated bboxes, points
-    :param img:
-    :param angle:
-    :param scale:
-    :param center: rotate center
-    :param bboxes:
-    :param points:
+    Affine transform the image(dense) and co-related points(sparse)
+    :param args: stage to cv2.warpAffine() and cv2.transform()
+    :param kwargs: stage to cv2.warpAffine() and cv2.transform()
     :return:
     """
     import cv2
-    if center is None:
-        center = img.shape[:2] / 2
-    if bboxes is None:
-        bboxes = []
-    if points is None:
-        points = []
-
-    _points = reduce(concat, chain(points, reduce(concat, bboxes)))
-    if len(points) > 0:
-        m = cv2.getRotationMatrix2D(center, angle, scale)
-        _points = cv2.transform(
-            f_compose_r(
-                np.array,
-                partial(np.expand_dims, axis=1),
-            )(_points),
-            m,
-        )
-        # transformed bboxes
-        _bboxes = np.array(_points[len(points):]).reshape([-1, 2, 2])
-        # transformed points
-        points = _points[:len(points)]
-        from labelme import utils
-    return points
+    return partial(_warp_transform, cv2.warpAffine, cv2.transform)(*args, **kwargs)
 
 
-def crop_img_by_bbox(img, bbox, bboxes = None, points = None):
+def warp_perspective_transform(*args, **kwargs):
+    """
+    Perspective transform the image(dense) and co-related points(sparse)
+    :param args: stage to cv2.warpPerspective() and cv2.perspectiveTransform()
+    :param kwargs: stage to cv2.warpPerspective() and cv2.perspectiveTransform()
+    :return:
+    """
+    import cv2
+    return partial(_warp_transform, cv2.warpPerspective, cv2.perspectiveTransform)(*args, **kwargs)
+
+
+def convert_bbox_to_rect(bbox):
+    """
+    Convert bbox format to standard 4 x points rect
+    :param bbox:
+    :return:
+    """
+    ul, dr = bbox
+    return ul, (ul[0], dr[1]), dr, (dr[0], ul[1])
+
+
+def convert_bounding_rect_to_bbox(bounding_rect) -> Tuple[tuple, tuple]:
+    """
+    Convert bounding_rect output to standard ((minx, miny), (maxx, maxy)) bbox
+    :param bounding_rect: cv2.boundingRect() output type
+    :return:
+    """
+    ul = bounding_rect[:2]
+    dr = (
+        ul[0] + bounding_rect[2],
+        ul[1] + bounding_rect[3],
+    )
+    return ul, dr
+
+
+def _biconvert_bboxes_with_rect(f_convert, items):
+    f = f_compose_r(
+        partial(partial, map),
+        partial(f_compose_l, list),
+    )
+    return f(f(f_convert))(items)
+
+
+def convert_bboxes_to_rectangles(*args, **kwargs):
+    """
+    Convert customized shaped bboxe array to rectangle array with same shape
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    return partial(_biconvert_bboxes_with_rect, convert_bbox_to_rect)(*args, **kwargs)
+
+
+def convert_rectangles_to_bboxes(*args, **kwargs):
+    """
+    Convert customized shaped rectangle array to bbox array with same shape
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    import cv2
+    return partial(_biconvert_bboxes_with_rect, f_compose_r(
+        cv2.boundingRect,
+        convert_bounding_rect_to_bbox,
+    ))(*args, **kwargs)
+
+
+def convert_rotated_rect_to_rect(rotated_rect) -> np.array:
+    """
+    Convert rotated_rect output to standard 4 x points rectangle
+    :param rotated_rect: cv2.minAreaRect() output type
+    :return:
+    """
+    import cv2
+
+    center, size, angle = rotated_rect
+    # TODO: why neg angle??? it works.
+    m = cv2.getRotationMatrix2D((0, 0), -angle, 1.0)
+    m[:, 2] = center
+    _, (pts,) = warp_affine_transform(m, points=[np.array(size)/2.0*[
+        [-1, -1],
+        [-1, 1],
+        [1, 1],
+        [1, -1],
+    ]])
+    return pts
+
+
+def crop_img_by_bboxes(mat, bboxes):
     """
     Crop image by a bounding box(rectangle)
     :param img:
     :param bbox:
-    :param bboxes:
-    :param points:
     :return:
     """
-    pass
+    return crop_img_by_polygons(mat, list(map(convert_bbox_to_rect, bboxes)))
 
 
-def crop_img_by_polygon(img, polygon, is_closed = False, bboxes = None, points = None):
-    pass
+def mask_img_by_polygons(mat, points):
+    import cv2
+
+    mask = np.zeros(mat.shape[:2], dtype="uint8")
+    _ = list(map(
+        f_compose_r(
+            methodcaller("astype", np.int32),
+            partial(cv2.fillConvexPoly, mask, color=255),
+        ),
+        points,
+    ))
+    import json
+    json.dump
+    return cv2.bitwise_and(mat, mat, mask=mask)
 
 
-def crop_img_by_rect(img, rect, bboxes = None, points = None):
-    pass
+def crop_img_by_polygons(mat, points, dsize=None):
+    import math
+    import cv2
+
+    x, y, w, h = cv2.boundingRect(points)
+
+    if dsize is None:
+        center, size, angle = cv2.minAreaRect(points)
+
+        d_width = f_compose_r(
+            np.square,
+            np.sum,
+            math.sqrt,
+            math.ceil,
+        )(size)
+        d_height = d_width
+        dsize = [d_width, d_height]
+
+    _y, _x = np.divide(np.subtract(dsize, [h, w]), 2).astype(np.int32)
+
+    canvas = np.zeros([*dsize[::-1], mat.shape[2]], dtype="uint8")
+    canvas[_y:_y+h, _x:_x+w] = mat[y:y+h, x:x+w]
+
+    m_translate = np.float32([[1, 0, _x-x], [0, 1, _y-y]])
+    _, (points,) = warp_affine_transform(
+        m_translate,
+        points=[points],
+    )
+    return mask_img_by_polygons(canvas, [points]), points, m_translate
 
 
-def scale_img(img, ratio, bboxes = None, points = None):
-    pass
+def dump_shape(mat, shape, transformers, orig_dict):
+    from labelme.utils import img_arr_to_b64
+
+    keys = set(orig_dict.keys()) - {"imageData", "shapes"}
+    info_dict = get_dict_filter_function_per_keys(keys)(orig_dict)
+
+    info_dict["shapes"] = [shape]
+    info_dict["imageData"] = img_arr_to_b64(mat)
+    info_dict["transformPipeline"] = list(map(methodcaller("tolist"), transformers))
+    return info_dict
 
 
+def rotate_img(angle, mat, points, center=None, *args, **kwargs):
+    import cv2
+    if center is None:
+        center = np.floor_divide(mat.shape[:2][::-1], 2)
+    m = cv2.getRotationMatrix2D(angle, center)
+    return warp_affine_transform(m, mat=mat, points=points, *args, **kwargs)
+
+
+def translate_img(mat, shift, points, *args, **kwargs):
+    w, h = shift
+    m = np.float32([[1, 0, w], [0, 1, h]])
+    return warp_affine_transform(m, mat=mat, points=points, *args, **kwargs)
